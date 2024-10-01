8f5c7ffed26553029ea02ad9d5b8d7dbe38c42bb
Shivansh Singh <140484999+shivrav123@users.noreply.github.com>
2024-10-01 06:16:30 +0530

    Update Greedy.java

--- Greedy.java
+++ Greedy.java
@@ -1,126 +1,126 @@
 // Activity Selection  (end time is sorted)
 
-// import java.util.*;
+ import java.util.*;
 
-// public class Greedy {
-// public static void main(String args[]) {
-//     int start[] = {1, 3, 0, 5, 8, 5};
-//     int end[] = {2, 4, 6, 7, 9, 9};
+public class Greedy {
+public static void main(String args[]) {
+    int start[] = {1, 3, 0, 5, 8, 5};
+    int end[] = {2, 4, 6, 7, 9, 9};
 
-//     //end time basis sorted
+    //end time basis sorted
 
-//     int maxAct = 0;
-//     ArrayList<Integer> ans = new ArrayList<>();
+    int maxAct = 0;
+    ArrayList<Integer> ans = new ArrayList<>();
     
-//     //1st activity
-//     maxAct = 1;
-//     ans.add(0);
+    //1st activity
+    maxAct = 1;
+    ans.add(0);
 
-//     int lastEnd = end[0];
-//     for(int i = 1; i < start.length; i++) {
-//         if(start[i] >= lastEnd) {
-//             //activity selection
-//             maxAct++;
-//             ans.add(i);
-//             lastEnd = end[i];
-//             }
-//         }
+    int lastEnd = end[0];
+    for(int i = 1; i < start.length; i++) {
+        if(start[i] >= lastEnd) {
+            //activity selection
+            maxAct++;
+            ans.add(i);
+            lastEnd = end[i];
+            }
+        }
 
-//         System.out.println("Max activities = " + maxAct);
-//         for(int i = 0; i < ans.size(); i++) {
-//             System.out.print("A" + ans.get(i) + " ");
-//         }
-//         System.out.println();
-//     }
-// }
+        System.out.println("Max activities = " + maxAct);
+        for(int i = 0; i < ans.size(); i++) {
+            System.out.print("A" + ans.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
 
 
 
 // Activity Selection  (end time is not sorted)
 
-// import java.util.Arrays;
-// import java.util.*;
+import java.util.Arrays;
+import java.util.*;
 
-// public class Greedy {
-//     public static void main(String args[]) {
-//         int start[] = {1, 3, 0, 5, 8, 5};
-//         int end[] = {2, 4, 6, 7, 9, 9};
+public class Greedy {
+    public static void main(String args[]) {
+        int start[] = {1, 3, 0, 5, 8, 5};
+        int end[] = {2, 4, 6, 7, 9, 9};
 
-//         //sorting
-//         int activities[][] = new int [start.length][3];
-//         for(int i = 0; i < start.length; i++) {
-//             activities[i][0] = i;
-//             activities[i][1] = start[i];
-//             activities[i][2] = end[i];
-//         }
+        //sorting
+        int activities[][] = new int [start.length][3];
+        for(int i = 0; i < start.length; i++) {
+            activities[i][0] = i;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
 
-//         //lambda function -> shortform
-//         Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));  // sorting based on end time
+        //lambda function -> shortform
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));  // sorting based on end time
 
-//         int maxAct = 0;
-//         ArrayList<Integer> ans = new ArrayList<>();
+        int maxAct = 0;
+        ArrayList<Integer> ans = new ArrayList<>();
 
-//         maxAct = 1;
-//         ans.add(activities[0][0]);
+        maxAct = 1;
+        ans.add(activities[0][0]);
         
-//         int lastEnd = activities[0][2];
-//         for(int i = 1; i < start.length; i++) {
-//             if(activities[i][1] >= lastEnd) {
-//                 maxAct++;
-//                 ans.add(activities[i][0]);
-//                 lastEnd = activities[i][2];
-//             }
-//         }
+        int lastEnd = activities[0][2];
+        for(int i = 1; i < start.length; i++) {
+            if(activities[i][1] >= lastEnd) {
+                maxAct++;
+                ans.add(activities[i][0]);
+                lastEnd = activities[i][2];
+            }
+        }
 
-//         System.out.println("max activities = " + maxAct);
-//         for(int i = 0; i < ans.size(); i++) {
-//             System.out.print("A" + ans.get(i) + " ");
-//         }
-//         System.out.println();
-//     }
-// }
+        System.out.println("max activities = " + maxAct);
+        for(int i = 0; i < ans.size(); i++) {
+            System.out.print("A" + ans.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
 
 
 
 //Fractional Knapsack
 
-// import java.util.*;
-// import java.util.Arrays;
+import java.util.*;
+import java.util.Arrays;
 
-// public class Greedy {
-//     public static void main(String args[]) {
-//         int value[] = {60, 100, 120};
-//         int weight[] = {10, 20, 30};
-//         int W = 50;
+public class Greedy {
+    public static void main(String args[]) {
+        int value[] = {60, 100, 120};
+        int weight[] = {10, 20, 30};
+        int W = 50;
 
-//         double ratio[][] = new double[value.length][2];
+        double ratio[][] = new double[value.length][2];
         
-//         for(int i = 0; i < value.length; i++) {
-//             ratio[i][0] = i;
-//             ratio[i][1] = value[i]/(double)weight[i];
-//         }
+        for(int i = 0; i < value.length; i++) {
+            ratio[i][0] = i;
+            ratio[i][1] = value[i]/(double)weight[i];
+        }
 
-//         //ascending order
-//         Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
+        //ascending order
+        Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
 
-//         int capacity = W;
-//         int finalValue = 0;
-//         for(int i = ratio.length - 1; i >= 0; i--) {
-//             int idx = (int)ratio[i][0];
-//             if(capacity >= weight[idx]) {  //include full item
-//                 finalValue += value[idx];
-//                 capacity -= weight[idx];
-//             } else {
-//                 //include fractional
-//                 finalValue += (ratio[i][1] * capacity);
-//                 capacity = 0;
-//                 break;
-//             }
-//         }
+        int capacity = W;
+        int finalValue = 0;
+        for(int i = ratio.length - 1; i >= 0; i--) {
+            int idx = (int)ratio[i][0];
+            if(capacity >= weight[idx]) {  //include full item
+                finalValue += value[idx];
+                capacity -= weight[idx];
+            } else {
+                //include fractional
+                finalValue += (ratio[i][1] * capacity);
+                capacity = 0;
+                break;
+            }
+        }
 
-//         System.out.println("final value is " + finalValue);
-//     }
-// }
+        System.out.println("final value is " + finalValue);
+    }
+}
 
 
 
