1f6b542e0af05f08aa934baa98b690532fc3458e
Shivansh Singh <140484999+shivrav123@users.noreply.github.com>
2024-11-07 17:11:08 +0530

    Update Backtracking.java

--- Backtracking.java
+++ Backtracking.java
@@ -29,35 +29,6 @@
 
 
 
-//find subsets  (we use string to add not string builder to append)
-
-// public class Backtracking {
-//     public static void findSubsets(String str, String ans, int i) {
-//         //base case
-//         if(i == str.length()) {
-            
-//             if(ans.length() == 0) {
-//                 System.out.println("null");
-//             } else {
-//                 System.out.println(ans);
-//             }
-//             return;
-//         }
-//         //yes choice to be the part of the subset
-//         findSubsets(str, ans + (str.charAt(i)), i + 1);
-        
-//         //no choice to be the part of the subset
-//         findSubsets(str, ans, i + 1);
-
-
-//     }
-//     public static void main(String args[]) {
-//        String str = "abc";
-//        findSubsets(str, "", 0);
-//     }
-// }
-
-
 
 //N-Queens 
 
@@ -248,86 +219,6 @@
 // }
 
 
-
-//nqueens print 1 solution
-
-
-// public class Backtracking {
-//     public static boolean isSafe(char board[][], int row, int col) {
-        
-//         for(int i = row - 1; i >= 0; i--) {
-//             if(board[i][col] == 'Q') {
-//                 return false;
-//             }
-//         }
-
-        
-//         for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-//             if(board[i][j] == 'Q') {
-//                 return false;
-//             }
-//         }
-
-        
-//         for(int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
-//             if(board[i][j] == 'Q') {
-//                 return false;
-//             }
-//         }
-        
-//         return true;
-//     }
-//     public static boolean nQueens(char board[][], int row) {   //
-        
-//         if(row == board.length) {
-//             return true;
-//         }
-
-        
-//         for(int j = 0; j < board.length; j++) {
-//             if(isSafe(board, row, j)) {
-//                 board[row][j] = 'Q';
-//                 if(nQueens(board, row + 1)) {    //
-//                     return true;
-//                 };  
-//                 board[row][j] = '*';  
-//             } 
-//         }
-
-//         return false;    //
-//     }
-
-//     public static void printBoard(char board [][]) {
-//         System.out.println("----------Chess Board------------");
-//         for(int i = 0; i < board.length; i++) {
-//             for(int j = 0; j < board.length; j++) {
-//                 System.out.print(board[i][j] + " ");
-//             }
-//             System.out.println();
-//         }
-//     }
-
-//     public static void main(String args[]) {
-//         int n = 4;
-//         char board[][] = new char[n][n];
-        
-        
-//         for(int i = 0; i < n; i++) {
-//             for(int j = 0; j < n; j++) {
-//                 board[i][j] = '*';
-//             }
-//         }
-
-//         nQueens(board, 0);
-
-//         if(nQueens(board, 0)) {   ///////////////////////////////
-//             System.out.println("solution is possible.");
-//             printBoard(board);
-//         } else {
-//             System.out.println("solution is not possible.");
-//         }
-//     }
-// }
 
 
 
